71366f0c7e672e2f88695d0dc56f7900e76adac3
bestprogramer69 <dsawcondeeznuts@gmail.com>
2023-04-24 22:22:34 +0200

    login fix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -20,23 +20,21 @@ export class LoginComponent {
     const hash = SHA256(this.password).toString();      
     //get hashed login from database
     this.http.get(`https://backendwerwirdmillionaer.azurewebsites.net/login/${this.user}`).subscribe((data: any) => {     
-      console.log(data);
-      if(!data){
-        alert('login not existing')
-        console.log('login not existing');
-        return;
-      }
-      else if ( data[0].password !== hash) {
-        alert('incorrect login')
-        console.log('incorrect login');
-        return;
-      }
-
+      
+    if (data && data[0] && data[0].password === hash) {
       //set to authorized      
       console.log('login correct');
-
       localStorage.setItem('authorized', 'true');
       this.router.navigate(['/', 'dashboard']);
+    } else if (data && data[0] && data[0].password !== hash) {
+      alert('incorrect login')
+      console.log('incorrect login');
+    } else {
+      alert('login not existing')
+      console.log('login not existing');
+    } 
+
+     
     }); 
 }
 }
